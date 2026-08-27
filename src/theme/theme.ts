e15764fd0c167dc47e57963a5b05b094d08edc4b
WH0M4N <whooman.t@gmail.com>
2026-08-27 16:25:47 +0330

    Initial easyArz setup

---
+++ src/theme/theme.ts
@@ -0,0 +1,16 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  direction: "rtl",
+
+  typography: {
+    fontFamily: "Arial, sans-serif",
+  },
+
+  palette: {
+    mode: "light",
+  },
+});
+
+export default theme;
