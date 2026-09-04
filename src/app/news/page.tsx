3d0c241a33551898b7f4a01add2eb33d40b360d5
WH0M4N <whooman.t@gmail.com>
2026-09-04 16:29:11 +0330

    Fix the navbar being duplicate in some pages

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,12 +1,9 @@
-import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import NewsList from "@/components/News/NewsList";
 
 export default function NewsPage() {
   return (
     <>
-      <Navbar />
-
       <main>
         <NewsList />
       </main>
